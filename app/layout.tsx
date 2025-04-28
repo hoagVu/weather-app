7f9c796ef57f1b810980069a5b93a2b6ca59c85c
hoang.vu2 <hoang.vu2@onemount.com>
2025-04-28 16:46:16 +0700

    fix bug ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,13 +40,13 @@ export default function RootLayout({
             appearance="light"
           >
             <div className="scroll-x-lg lg:bg-gray-200 h-screen w-screen flex items-center justify-center p-0 lg:p-[100px]">
-              <main className="flex flex-col lg:grid lg:grid-cols-12 h-screen w-screen lg:w-full lg:h-full bg-slate-50 lg:border lg:border-gray-100 max-w-[1240px] lg:max-h-[576px] lg:rounded-xl">
+              <main className="flex flex-col lg:grid lg:grid-cols-12 w-full h-full bg-slate-50 lg:border lg:border-gray-100 max-w-[1240px] lg:rounded-xl">
                 <div className="w-full p-4 lg:p-6 col-span-4 flex flex-col pb-6 lg:pr-6 gap-4">
                   <Search />
                   <WeatherOverview />
                 </div>
 
-                <div className="p-4 lg:p-6 col-span-8 bg-slate-100 flex max-h-[calc(100%-24px)] flex-col h-full lg:pl-6">
+                <div className="p-4 lg:p-6 col-span-8 bg-slate-100 flex flex-col h-full pb-[30px] lg:pl-6">
                   {children}
                 </div>
               </main>
